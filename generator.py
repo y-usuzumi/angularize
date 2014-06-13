3a88baf27c1fcf5d24dd800676162dcf16c3f35e
Savor d'Isavano <newelevenken@aliyun.com>
2014-06-13 16:36:30 +0800

    Can do basic javascript parsing.

--- generator.py
+++ generator.py
@@ -15,15 +15,18 @@ def class_stringify(cls):
     else:
         raise TypeError("%s is not a type", cls)
 
-class ASTTranslator:
+class AstTranslator:
     def __init__(self, code):
         self.code = code
 
-    def __node_translate(self, node):
+    def _node_translate(self, node):
         t = type(node)
-        translator_name = "__%s_translate" % \
-                          class_stringify(t.mro()[-3]) # dirty work
-        self.__dict__[translator_name](node)
+        if issubclass(t, list):
+            node = node[0]
+            t = type(node)
+        translator_name = "_%s_translate" % \
+                           class_stringify(t.mro()[-3]) # dirty work
+        return getattr(self, translator_name)(node)
 
 ## TODO: 敢不敢把这玩意放元类里？
 
@@ -32,26 +35,56 @@ class ASTTranslator:
             'mod', 'stmt', 'expr', 'expr_context',
             'slice', 'boolop', 'operator', 'unaryop',
             'compop', 'comprehension', 'excepthandler',
-            'arguments', 'keyword', 'alias'
     ]:
         exec(
 '''
-def __{0}_translate(self, node):
+def _{0}_translate(self, node):
     t = type(node)
-    translator_name = "__%s_translate" % \
-                      class_stringify(t.mro()[-4]) # dirty work
-    self.__dict__[translator_name](node)
+    translator_name = "_%s_translate" % \
+                       class_stringify(t.mro()[-4]) # dirty work
+
+    return getattr(self, translator_name)(node)
 '''.format(group))
 
+    def _arg_translate(self, node):
+        return node.arg
 
-def node_translate(node):
-    t = type(node)
-    if issubclass(t, ast.FuntionDef):
+    def _arguments_translate(self, node):
+        _templ = "{0}"
+        return ', '.join([_templ.format(self._node_translate(arg)) for arg in node.args])
+
+    def _FunctionDef_translate(self, node):
         _templ = "function {0}({1}) {{ {2} }}"
-        return _templ.format(t.name, node_translate(t.args), func_body_translate(body))
+        return _templ.format(node.name, self._node_translate(node.args), self._node_translate(node.body))
+
+    def _Return_translate(self, node):
+        _templ = "return {0};"
+        if hasattr(node.value, 'n'):
+            return _templ.format(node.value.n)
+        else:
+            return _templ.format(self._node_translate(node.value));
+
+    def _BinOp_translate(self, node):
+        _templ = "{0} {1} {2}"
+        return _templ.format(self._node_translate(node.left), self._node_translate(node.op), self._node_translate(node.right))
+
+    def _Name_translate(self, node):
+        _templ = "{0}"
+        return _templ.format(node.id)
+
+    def _Add_translate(self, node):
+        _templ = "{0}"
+        return _templ.format("+")
+
+    def node_translate(self, node):
+        if isinstance(node, str):
+            node = ast.parse(node).body[0]
+            
+        return self._node_translate(node)
             
 
 def func_body_translate(body):
+    return "TODO"
     for node in body:
         s = node_translate(node)
 
--- test/test_ast.py
+++ test/test_ast.py
@@ -1,6 +1,6 @@
 import unittest
 
-class TestAST(unittest.TestCase):
+class TestAst(unittest.TestCase):
     def setUp(self):
         pass
 
@@ -10,8 +10,26 @@ class TestAST(unittest.TestCase):
         self.assertTrue(issubclassofany(defaultdict, [dict, int, str]))
         self.assertFalse(issubclassofany(defaultdict, [int, str, float]))
 
+    def test_class_stringify(self):
+        from generator import class_stringify
+        import ast
+        self.assertEqual(class_stringify(ast.stmt), 'stmt')
+        self.assertEqual(class_stringify(ast.Module), 'Module')
+        self.assertEqual(class_stringify(int), 'int')
+
+    def test_ast_translator_methods_created_properly(self):
+        from generator import AstTranslator
+        self.assertTrue('_stmt_translate' in AstTranslator.__dict__)
+        translator = AstTranslator(None)
+        self.assertTrue(getattr(translator, '_stmt_translate') is not None)
+        
     def test_ast_mro(self):
         import ast
         lv1_type = ast.Assign.mro()[-3]
         self.assertTrue(lv1_type is ast.stmt)
                         
+    def test_ast_translator_translate_functions(self):
+        from generator import AstTranslator
+        code = '''def func(a, b): return a + b'''
+        translator = AstTranslator(code)
+        print(translator.node_translate(code))
